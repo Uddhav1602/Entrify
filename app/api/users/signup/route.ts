d1f9fdb83781f7f315af0083c645f1beebb5c715
Uddhav <vidhateuddhav6271@gmail.com>
2026-01-30 12:02:29 +0530

    bug fix

--- app/api/users/signup/route.ts
+++ app/api/users/signup/route.ts
@@ -1,4 +1,4 @@
-import {connect} from "@/dbConfig/dbConfig";
+import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
@@ -6,45 +6,45 @@ import { sendEmail } from "@/helpers/mailer";
 
 connect();
 
-export async function POST(request: NextRequest){
-    try {
-        const reqBody = await request.json();
-        const {username, email, password} = reqBody;
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
 
-        console.log(reqBody);
+    console.log(reqBody);
 
-        // Check if user already exists
-        const user = await User.findOne({email})
-        if(user){
-            return NextResponse.json({error: "User already exists"}, {status: 400})
-        }
+    // Check if user already exists
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 },
+      );
+    }
 
-        //hash password
-        const salt = await bcrypt.genSalt(10); //bcrypt.hash() is a function that combines (mixes) your password with a generated salt and then applies multiple hashing rounds to produce a secure, irreversible hash.
-        const hashedPassword = await bcrypt.hash(password, salt);
+    //hash password
+    const salt = await bcrypt.genSalt(10); //bcrypt.hash() is a function that combines (mixes) your password with a generated salt and then applies multiple hashing rounds to produce a secure, irreversible hash.
+    const hashedPassword = await bcrypt.hash(password, salt);
 
+    //create new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
 
-        //create new user
-        const newUser = new User({
-            username,
-            email,
-            password: hashedPassword
-        })
+    const savedUser = await newUser.save();
+    console.log(savedUser);
 
-        const savedUser =await newUser.save();
-        console.log(savedUser);
+    //send verification email
+    await sendEmail({ email, emailType: "VERIFY", userId: savedUser._id });
 
-        //send verification email
-        await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
-
-        return NextResponse.json({
-            message: "User created successfully",
-            success: true,
-            savedUser
-        })
-    } catch (error) {
-        return NextResponse.json({error: error.message},
-            {status: 500}
-        )
-    }    
+    return NextResponse.json({
+      message: "User created successfully",
+      success: true,
+      savedUser,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
